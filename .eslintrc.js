8cecf3e4362ec85c64d62cc5fc831fcb9e45758a
Ivo <ivoprovensi1@gmail.com>
2021-04-01 20:33:31 -0400

    fix: 🐛 eslint
    now eslint works as it should

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,45 @@
-// .eslintrc.js
 module.exports = {
   root: true,
-  env: {
-    node: true,
-    es6: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
   },
-  parserOptions: { ecmaVersion: 8 }, // to enable features such as async/await
-  ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '!.prettierrc.js'], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
-  extends: ['eslint:recommended'],
-  overrides: [
-    // This configuration will apply only to TypeScript files
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      parser: '@typescript-eslint/parser',
-      settings: { react: { version: 'detect' } },
-      env: {
-        browser: true,
-        node: true,
-        es6: true,
-      },
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended', // TypeScript rules
-        'plugin:react/recommended', // React rules
-        'plugin:react-hooks/recommended', // React hooks rules
-        'plugin:jsx-a11y/recommended', // Accessibility rules
-        'plugin:prettier/recommended', // Prettier plugin
-      ],
-      rules: {
-        // We will use TypeScript's types for component props instead
-        'react/prop-types': 'off',
-
-        // No need to import React when using Next.js
-        'react/react-in-jsx-scope': 'off',
-
-        // This rule is not compatible with Next.js's <Link /> components
-        'jsx-a11y/anchor-is-valid': 'off',
-
-        // Why would you want unused vars?
-        '@typescript-eslint/no-unused-vars': ['error'],
-
-        // I suggest this setting for requiring return types on functions only where useful
-        '@typescript-eslint/explicit-function-return-type': [
-          'warn',
-          {
-            allowExpressions: true,
-            allowConciseArrowFunctionExpressionsStartingWithVoid: true,
-          },
-        ],
-
-        // Includes .prettierrc.js rules
-        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-      },
-    },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true
+  },
+  plugins: ["simple-import-sort"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended" // Make sure this is always the last element in the array.
   ],
-}
+  rules: {
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "simple-import-sort/imports": "error",
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"]
+      }
+    ]
+  }
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-export default function Home() {
-  return (
-    <h1>dashgo</h1>
-  )
+export default function Home(): JSX.Element {
+  return <h1>dashgo</h1>;
 }
